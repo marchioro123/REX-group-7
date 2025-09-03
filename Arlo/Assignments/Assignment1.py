c2e1bb95f3acd70d206189225b77c441f66df4e3
Martin <martin.marchioro@gmail.com>
2025-09-03 14:13:07 +0200

    .

--- Arlo/Assignments/Assignment1.py
+++ Arlo/Assignments/Assignment1.py
@@ -20,7 +20,7 @@ while (1):
     print(arlo.go_diff(round(40*leftPercentageSpeed), 115, 1, 1))
     sleep(4.5)
     print(arlo.go_diff(round(115*leftPercentageSpeed), 40, 1, 1))
-    sleep(4.3)
+    sleep(4.5)
 
 
 
