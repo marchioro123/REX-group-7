cfcf5aede9cf247c0b5e2de86bc854ec1e72cd8d
Martin <martin.marchioro@gmail.com>
2025-09-08 20:37:04 +0200

    .

--- Arlo/python/Exercises/Ex2-1.py
+++ Arlo/python/Exercises/Ex2-1.py
@@ -11,19 +11,21 @@ cmd_queue = queue.Queue()
 motor = MotorThread(arlo, cmd_queue)
 motor.start()
 
+cmd_queue.put(("drive_n_cm_forward", 0, 10000))
 while True:
     front_dist = arlo.read_front_ping_sensor()
-    if (front_dist == -1 or front_dist < 500):
-        cmd_queue.put(("drive_n_cm_forward", 0, 5))
-    else:
-        left_dist = arlo.read_front_ping_sensor()
-        right_dist = arlo.read_front_ping_sensor()
+    if front_dist != -1 and front_dist < 500:
+        motor.hard_stop()
 
-        if not (left_dist == -1 or left_dist < 500):
-            cmd_queue.put(("turn_90_degrees_left", 0))
-        elif not (right_dist == -1 or right_dist < 500):
-            cmd_queue.put(("turn_n_degrees_right", 90))
+        left_dist = arlo.read_left_ping_sensor()
+        right_dist = arlo.read_right_ping_sensor()
+
+        if left_dist != -1 and left_dist < 500:
+            cmd_queue.put(("turn_90_degrees", 0))
+        elif right_dist != -1 and right_dist < 500:
+            cmd_queue.put(("turn_90_degrees", 1))
         else:
-            cmd_queue.put(("turn_n_degrees_right", 180))
+            cmd_queue.put(("turn_90_degrees", 1))
+        cmd_queue.put(("drive_n_cm_forward", 0, 10000))
 
     time.sleep(0.01)
--- Arlo/python/motor_thread.py
+++ Arlo/python/motor_thread.py
@@ -2,7 +2,6 @@ import threading
 import queue
 import time
 
-
 class MotorThread(threading.Thread):
     def __init__(self, arlo, cmd_queue: queue.Queue):
         super().__init__(daemon=True)
@@ -12,21 +11,19 @@ class MotorThread(threading.Thread):
 
     def run(self):
         while True:
-            try:
-                if self.wait_until <= time.monotonic():
-                    name, *args = self.cmd_queue.get(block=False)
+            if self.wait_until and time.monotonic() >= self.wait_until:
+                self.arlo.stop()
+                self.wait_until = 0
 
-                    if name == "turn_90_degrees_left":
-                        self._turn_90_degrees_left()
-                    elif name == "turn_n_degrees_right":
-                        self._turn_n_degrees_right(float(args[0]))
+            if self.wait_until == 0:
+                try:
+                    name, *args = self.cmd_queue.get(block=False)
+                    if name == "turn_90_degrees":
+                        self._turn_90_degrees(*args)
                     elif name == "drive_n_cm_forward":
                         self._drive_n_cm_forward(*args)
-
-            except queue.Empty:
-                if self.wait_until != 0:
-                    self.arlo.stop()
-                    self.wait_until = 0
+                except queue.Empty:
+                    pass
 
             time.sleep(0.01)
 
@@ -36,55 +33,33 @@ class MotorThread(threading.Thread):
                 self.cmd_queue.get(block=False)
             except queue.Empty:
                 break
-        self.arlo.stop()
         self.wait_until = 0
+        self.arlo.stop()
 
+    def _turn_90_degrees(self, direction: int):
+        if direction == 0:
+            LEFTSPEED, RIGHTSPEED = 105, 100
+            self.arlo.go_diff(LEFTSPEED, RIGHTSPEED, 0, 1)
+            duration = 0.37
+        else:
+            LEFTSPEED, RIGHTSPEED = 105, 100
+            self.arlo.go_diff(LEFTSPEED, RIGHTSPEED, 1, 0)
+            duration = 0.37
 
-    def _turn_90_degrees_left(self):
-        LEFTSPEED, RIGHTSPEED = 105, 100
-        self.arlo.go_diff(LEFTSPEED, RIGHTSPEED, 0, 1)
-
-        duration = 0.37
-        self.wait_until = time.monotonic() + duration
-
-
-    def _turn_n_degrees_right(self, n: float):
-        LEFTSPEED, RIGHTSPEED = 105, 100
-        self.arlo.go_diff(LEFTSPEED, RIGHTSPEED, 1, 0)
-
-        duration = 1.65 * n / 360.0
         self.wait_until = time.monotonic() + duration
 
     def _drive_n_cm_forward(self, speed: int, cm: float):
         if speed == 0:
             LEFTSPEED, RIGHTSPEED = 41, 40
             self.arlo.go_diff(LEFTSPEED, RIGHTSPEED, 1, 1)
-
             duration = 0.039 * cm
-            self.wait_until = time.monotonic() + duration
         elif speed == 1:
             LEFTSPEED, RIGHTSPEED = 86, 83
             self.arlo.go_diff(LEFTSPEED, RIGHTSPEED, 1, 1)
-
             duration = 0.0162 * cm
-            self.wait_until = time.monotonic() + duration
         else:
             LEFTSPEED, RIGHTSPEED = 127, 115
             self.arlo.go_diff(LEFTSPEED, RIGHTSPEED, 1, 1)
-
             duration = 0.0105 * cm
-            self.wait_until = time.monotonic() + duration
-    
-    # def _drive_circle_right(self):
-    #     LEFTSPEED, RIGHTSPEED = 127, 40
-    #     self.arlo.go_diff(LEFTSPEED, RIGHTSPEED, 1, 1)
 
-    #     duration = 4.3
-    #     self.wait_until = time.monotonic() + duration
-
-    # def _drive_circle_left(self):
-    #     LEFTSPEED, RIGHTSPEED = 44, 115
-    #     self.arlo.go_diff(LEFTSPEED, RIGHTSPEED, 1, 1)
-
-    #     duration = 4.4
-    #     self.wait_until = time.monotonic() + duration
+        self.wait_until = time.monotonic() + duration
