78376257cec909eea12e1a8b13b18ee2ddf942ee
Martin <martin.marchioro@gmail.com>
2025-09-08 16:24:28 +0200

    .

--- Arlo/python/motor_thread.py
+++ Arlo/python/motor_thread.py
@@ -44,7 +44,7 @@ class MotorThread(threading.Thread):
         LEFTSPEED, RIGHTSPEED = 105, 100
         self.arlo.go_diff(LEFTSPEED, RIGHTSPEED, 0, 1)
 
-        duration = 0.41
+        duration = 0.37
         self.wait_until = time.monotonic() + duration
 
 
