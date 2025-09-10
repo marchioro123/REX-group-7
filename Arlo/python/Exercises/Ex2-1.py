dd247ebea0d5b5bdfb563635edb44bea8a2785fd
Martin <martin.marchioro@gmail.com>
2025-09-10 12:29:28 +0200

    .

--- Arlo/python/Exercises/Ex2-1.py
+++ Arlo/python/Exercises/Ex2-1.py
@@ -15,11 +15,14 @@ cmd_queue.put(("drive_n_cm_forward", 0, 10000))
 while True:
     if cmd_queue.empty():
         front_dist = arlo.read_front_ping_sensor()
+        time.sleep(0.01)
         if front_dist != -1 and front_dist < 500:
             motor.hard_stop()
 
             left_dist = arlo.read_left_ping_sensor()
+            time.sleep(0.01)
             right_dist = arlo.read_right_ping_sensor()
+            time.sleep(0.01)
 
             if left_dist != -1 and left_dist < 500:
                 cmd_queue.put(("turn_90_degrees", 0))
