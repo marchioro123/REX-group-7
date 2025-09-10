067eca838185e043b307529c072ad19812d50522
Martin <martin.marchioro@gmail.com>
2025-09-10 13:12:50 +0200

    .

--- Arlo/python/Exercises/Ex2-1.py
+++ Arlo/python/Exercises/Ex2-1.py
@@ -1,38 +1,42 @@
 import time
 import queue
 import sys
+import threading
 sys.path.append("..")
 import robot
 from motor_thread import MotorThread
 
+SERIAL_LOCK = threading.Lock()
+
 arlo = robot.Robot()
+queue_empty = True
 
 cmd_queue = queue.Queue()
-motor = MotorThread(arlo, cmd_queue)
+motor = MotorThread(arlo, cmd_queue, serial_lock=SERIAL_LOCK)
 motor.start()
 
 cmd_queue.put(("drive_n_cm_forward", 0, 10000))
-time.sleep(0.01)
+
 while True:
-    if cmd_queue.empty():
-        front_dist = arlo.read_front_ping_sensor()
+    if motor.is_busy():
         time.sleep(0.01)
-        if front_dist != -1 and front_dist < 500:
-            motor.hard_stop()
+        continue
 
-            # left_dist = arlo.read_left_ping_sensor()
-            # time.sleep(0.01)
-            # right_dist = arlo.read_right_ping_sensor()
-            # time.sleep(0.01)
+    with SERIAL_LOCK:
+        front_dist = arlo.read_front_ping_sensor()
+    if front_dist != -1 and front_dist < 500:
+        motor.hard_stop()
+        with SERIAL_LOCK:
+            left_dist = arlo.read_left_ping_sensor()
+        with SERIAL_LOCK:
+            right_dist = arlo.read_right_ping_sensor()
 
-            # if left_dist != -1 and left_dist < 500:
-            #     cmd_queue.put(("turn_90_degrees", 0))
-            # elif right_dist != -1 and right_dist < 500:
-            #     cmd_queue.put(("turn_90_degrees", 1))
-            # else:
-            #     cmd_queue.put(("turn_90_degrees", 1))
-            # time.sleep(0.01)
-            # cmd_queue.put(("drive_n_cm_forward", 0, 10000))
-            # time.sleep(0.01)
+        if left_dist != -1 and left_dist < 500:
+            cmd_queue.put(("turn_90_degrees", 0, True))
+        elif right_dist != -1 and right_dist < 500:
+            cmd_queue.put(("turn_90_degrees", 1, True))
+        else:
+            cmd_queue.put(("turn_90_degrees", 1, True))
+        cmd_queue.put(("drive_n_cm_forward", 0, 10000))
 
     time.sleep(0.01)
--- Arlo/python/motor_thread.py
+++ Arlo/python/motor_thread.py
@@ -3,10 +3,12 @@ import queue
 import time
 
 class MotorThread(threading.Thread):
-    def __init__(self, arlo, cmd_queue: queue.Queue):
+    def __init__(self, arlo, cmd_queue: queue.Queue, serial_lock: threading.Lock):
         super().__init__(daemon=True)
         self.arlo = arlo
         self.cmd_queue = cmd_queue
+        self.serial_lock = serial_lock
+
         self.wait_until = 0
         self.uninterruptible = False
 
@@ -23,13 +25,17 @@ class MotorThread(threading.Thread):
 
             except queue.Empty:
                 if self.wait_until != 0:
-                    self.arlo.stop()
+                    with self.serial_lock:
+                        self.arlo.stop()
                     self.wait_until = 0
                     self.uninterruptible = False
 
 
             time.sleep(0.01)
 
+    def is_busy(self) -> bool:
+        return not self.uninterruptible
+    
     def hard_stop(self):
         if self.uninterruptible:
             return
@@ -40,16 +46,19 @@ class MotorThread(threading.Thread):
             except queue.Empty:
                 break
         self.wait_until = 0
-        self.arlo.stop()
+        with self.serial_lock:
+            self.arlo.stop()
 
     def _turn_90_degrees(self, direction: int, uninterruptible = False):
         if direction == 0:
             LEFTSPEED, RIGHTSPEED = 105, 100
-            self.arlo.go_diff(LEFTSPEED, RIGHTSPEED, 0, 1)
+            with self.serial_lock:
+                self.arlo.go_diff(LEFTSPEED, RIGHTSPEED, 0, 1)
             duration = 0.37
         else:
             LEFTSPEED, RIGHTSPEED = 105, 100
-            self.arlo.go_diff(LEFTSPEED, RIGHTSPEED, 1, 0)
+            with self.serial_lock:
+                self.arlo.go_diff(LEFTSPEED, RIGHTSPEED, 1, 0)
             duration = 0.37
 
         self.uninterruptible = uninterruptible
@@ -58,15 +67,18 @@ class MotorThread(threading.Thread):
     def _drive_n_cm_forward(self, speed: int, cm: float, uninterruptible = False):
         if speed == 0:
             LEFTSPEED, RIGHTSPEED = 41, 40
-            self.arlo.go_diff(LEFTSPEED, RIGHTSPEED, 1, 1)
+            with self.serial_lock:
+                self.arlo.go_diff(LEFTSPEED, RIGHTSPEED, 1, 1)
             duration = 0.039 * cm
         elif speed == 1:
             LEFTSPEED, RIGHTSPEED = 86, 83
-            self.arlo.go_diff(LEFTSPEED, RIGHTSPEED, 1, 1)
+            with self.serial_lock:
+                self.arlo.go_diff(LEFTSPEED, RIGHTSPEED, 1, 1)
             duration = 0.0162 * cm
         else:
             LEFTSPEED, RIGHTSPEED = 127, 115
-            self.arlo.go_diff(LEFTSPEED, RIGHTSPEED, 1, 1)
+            with self.serial_lock:
+                self.arlo.go_diff(LEFTSPEED, RIGHTSPEED, 1, 1)
             duration = 0.0105 * cm
 
         self.uninterruptible = uninterruptible
