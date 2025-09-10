1f3a7dcaaf6ad54e99874642503a7907c7243134
Martin <martin.marchioro@gmail.com>
2025-09-10 17:04:28 +0200

    .

--- Arlo/python/Exercises/Ex2-1.py
+++ Arlo/python/Exercises/Ex2-1.py
@@ -16,22 +16,20 @@ cmd_queue = queue.Queue()
 motor = MotorThread(arlo, cmd_queue, serial_lock=SERIAL_LOCK)
 motor.start()
 
-cmd_queue.put(("drive_n_cm_forward", 0, 10000))
 while True:
-    if motor.is_busy():
-        time.sleep(0.05)
-        continue
-
     with SERIAL_LOCK:
         front_dist = arlo.read_front_ping_sensor()
         left_dist = arlo.read_left_ping_sensor()
         right_dist = arlo.read_right_ping_sensor()
     if should_stop(front_dist, left_dist, right_dist):
+        if not motor.is_turning():
+            motor.hard_stop()
+            if left_dist != -1 and left_dist < 500:
+                cmd_queue.put(("turn", 1, 999))
+            else:
+                cmd_queue.put(("turn", 0, 999))
+    elif not motor.is_driving_forward():
         motor.hard_stop()
-        if left_dist != -1 and left_dist < 500:
-            cmd_queue.put(("turn_90_degrees", 1, True))
-        else:
-            cmd_queue.put(("turn_90_degrees", 0, True))
         cmd_queue.put(("drive_n_cm_forward", 0, 10000))
 
     time.sleep(0.05)
