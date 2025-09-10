3086f23c62610cfe20792000e5b2dad50d15b6a1
Martin Fof <martin.fof1@seznam.cz>
2025-09-10 12:48:24 +0200

    repaired something

--- Arlo/python/shared_files/something.py
+++ Arlo/python/shared_files/something.py
@@ -1,26 +1,43 @@
 from time import sleep, time
-
 from collections import deque
+import robot
 
 
-def move_straight(l):
-    # computes time to go straight, assuming 1 meter = 1.8 second
-    time = l*1.8
+arlo = robot.Robot()
 
-    print(arlo.go_diff(68, 64, 1, 1))
+
+def move_straight(l, speed = "medium"):
+    if speed == "slow":
+        print(arlo.go_diff(41, 40, 1, 1))
+        time = l*0.039
+    if speed == "medium":
+        print(arlo.go_diff(86, 83, 1, 1))
+        time = l*0.0162
+    if speed == "fast":
+        print(arlo.go_diff(127, 115, 1, 1))
+        time = l*0.0105
+
     wait_till = time() + time
     return wait_till
 
 def rotate_left(alpha):
     # computes time to rotate, assuming 90 degrees = 0.71 second
-    time = alpha/90*0.71
+    time = 0.37/90*alpha
+
+    print(arlo.go_diff(105, 100, 0, 1))
+    wait_till = time() + time
+    return wait_till
+
+def rotate_right(alpha):
+    # computes time to rotate, assuming 90 degrees = 0.71 second
+    time = alpha/90*0.73
 
     print(arlo.go_diff(68, 64, 0, 1))
     wait_till = time() + time
     return wait_till
 
 que = deque()
-que.extend([(move_straight, 1), (rotate_left, 90)]*4)
+que.extend([(move_straight, 100), (rotate_left, 90)]*4)
 
 
 wait_till = time()
