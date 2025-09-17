a2b9565c216afbaa099a499ba1f4dcb1d5b8a5d3
Martin Fof <martin.fof1@seznam.cz>
2025-09-17 14:25:57 +0200

    .

--- Arlo/python/Exercises/Ex3-3.py
+++ Arlo/python/Exercises/Ex3-3.py
@@ -1,6 +1,8 @@
 import sys
 from time import sleep
 
+import matplotlib as plt
+
 sys.path.append("..")
 import robot
 from camera import cam, find_corner_coordinates, detector
@@ -10,7 +12,13 @@ image = cam.capture_array("main")
 corners, ids, rejected = detector.detectMarkers(image)
 rvecs, tvecs, _ = find_corner_coordinates(corners)
 
-
+x_es = []
+z_es = []
 for code in tvecs:
     x, _, z = code[0]
     print(x,z)
+    x_es.append(x)
+    z_es.append(z)
+
+plt.scatter(x_es,z_es)
+plt.show()
