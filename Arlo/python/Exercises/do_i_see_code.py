be86313a3db0b1387265b02687ef7dee590d4c6c
Martin Fof <martin.fof1@seznam.cz>
2025-09-17 13:48:15 +0200

    .

--- Arlo/python/Exercises/do_i_see_code.py
+++ Arlo/python/Exercises/do_i_see_code.py
@@ -22,7 +22,7 @@ while found_end is False:
     if ids is None:
         arlo.go_diff(40,40,1,0)
         print("rotato")
-        sleep(0.05)
+        sleep(0.2)
         arlo.stop()
     else:
         arlo.stop()
