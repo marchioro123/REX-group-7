6cf41ac1ae2c67f1910a1dce91ba8f0f90209f5a
Martin Fof <martin.fof1@seznam.cz>
2025-09-22 14:04:45 +0200

    .

--- Arlo/python/Exercises/Ex4-1.py
+++ Arlo/python/Exercises/Ex4-1.py
@@ -14,21 +14,22 @@ image = cam.capture_array("main")
 corners, ids, rejected = detector.detectMarkers(image)
 rvecs, tvecs, _ = find_corner_coordinates(corners)
 
-x_es = []
-z_es = []
-for code in tvecs:
-    x, _, z = code[0]
-    print(x,z)
-    print(rvecs)
-    print("---------------")
-    x_es.append(x)
-    z_es.append(z)
-
-maximum_absolute_value = max(abs(x) for x in x_es)
 
 _, graph = plt.subplots(figsize=(5, 5))
 
-graph.scatter(x_es,z_es, color = "blue")
+x_es = []
+z_es = []
+for i in range(tvecs):
+    x, _, z = tvecs[i][0]
+    x_dir, _, z_dir = rvecs[i][0]
+    print(x,z)
+    x_es.append(x)
+    z_es.append(z)
+    graph.scatter(x, z, color = "blue")
+    graph.quiver([x,z],[x_dir,z_dir])
+
+maximum_absolute_value = max(abs(x) for x in x_es)
+
 circle = patches.Circle((0, -0.225), radius=0.225, color='black', fill=True)
 graph.add_patch(circle)
 
