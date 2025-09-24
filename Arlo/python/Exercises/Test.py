3161902131d529715b03f92b15c97b241ee931e8
Martin <martin.marchioro@gmail.com>
2025-09-24 12:55:11 +0200

    .

--- Arlo/python/Exercises/Test.py
+++ Arlo/python/Exercises/Test.py
@@ -1,57 +1,70 @@
+import sys
+from time import sleep
 import numpy as np
+
 import matplotlib.pyplot as plt
-from scipy.stats import norm
+import matplotlib.patches as patches
+import cv2
+from math import sin, cos, sqrt
 
-def p(x):
-    return (0.3 * norm.pdf(x, loc=2.0, scale=1.0) +
-            0.4 * norm.pdf(x, loc=5.0, scale=2.0) +
-            0.3 * norm.pdf(x, loc=9.0, scale=1.0))
+sys.path.append("..")
+from utils import find_corner_coordinates, detector
 
-# SIR uniform distribution
-def sir_uniform(k, rng=np.random.default_rng()):
-    samples = rng.uniform(0, 15, size=k)
-    weights = p(samples)
-    weightsN = weights / np.sum(weights)
-    return rng.choice(samples, size=k, replace=True, p=weightsN)
+DISTANCE_TO_CENTER = 0.1
+BOX_RADIUS = 0.18
+ROBOT_RADIUS = 0.2250
 
 
-# SIR normal distribution - N(5,4)
-def sir_normal(k, rng=np.random.default_rng()):
-    samples = rng.normal(loc=5, scale=2, size=k)
-    weights = p(samples) / norm.pdf(samples, loc=5, scale=2)
-    weightsN = weights / np.sum(weights)
-    return rng.choice(samples, size=k, replace=True, p=weightsN)
+def Collided(x, y, obstacle_centers):
+    crashed = False
+    for circle in obstacle_centers:
+        if sqrt((x-circle[0])**2+(y-circle[1])**2) <= ROBOT_RADIUS + BOX_RADIUS:
+            crashed = True
+            break
+    return crashed
+        
 
 
-# Plot results for k = 20, 100, 1000, 10000
-ks = [20, 100, 1000, 10000]
-x = np.linspace(-2, 15, 1000)
-px = p(x)
+image = cv2.imread("../../../Images/frame.png")
 
-for k in ks:
-    # Generate resampled data
-    resampled_uniform = sir_uniform(k)
-    resampled_normal = sir_normal(k)
-    
-    # ---- Plot for uniform proposal ----
-    plt.figure(figsize=(7, 4))
-    plt.hist(resampled_uniform, bins=50, density=True, alpha=0.6, label=f"SIR resampled uniform (k={k})")
-    plt.plot(x, px, "r-", lw=2, label="True distribution p(x)")
-    plt.xlabel("x")
-    plt.ylabel("Probability Density")
-    plt.title(f"SIR with uniform proposal, k={k}")
-    plt.legend()
-    plt.show()
-    
-    # ---- Plot for normal proposal ----
-    plt.figure(figsize=(7, 4))
-    plt.hist(resampled_normal, color="orange", bins=50, density=True, alpha=0.6, label=f"SIR resampled normal (k={k})")
-    plt.plot(x, px, "r-", lw=2, label="True distribution p(x)")
-    plt.xlabel("x")
-    plt.ylabel("Probability Density")
-    plt.title(f"SIR with normal proposal, k={k}")
-    plt.legend()
-    plt.show()
+corners, ids, rejected = detector.detectMarkers(image)
+rvecs, tvecs, _ = find_corner_coordinates(corners)
 
 
+_, graph = plt.subplots(figsize=(5, 5))
 
+x_es = []
+z_es = []
+obstacle_centers = []
+for i in range(len(ids)):
+    x, y, z = tvecs[i][0]
+    x_dir, _, z_dir = rvecs[i][0]
+    print(corners[i])
+    print(ids[i])
+    print(x,y,z)
+    print(rvecs[i])
+    print("--------------")
+    x_es.append(x)
+    z_es.append(z)
+    graph.scatter(x, z, color = "blue")
+    graph.quiver(x,z, sin(z_dir/2), -cos(z_dir/2))
+    obstacle_center = (x-sin(z_dir/2)*DISTANCE_TO_CENTER, z+cos(z_dir/2)*DISTANCE_TO_CENTER)
+    obstacle_centers.append(obstacle_center)
+    obstacle_circle = patches.Circle(obstacle_center, radius=BOX_RADIUS, color='red', fill=True)
+    graph.add_patch(obstacle_circle)
+
+maximum_absolute_value = max(abs(x) for x in x_es)
+
+circle = patches.Circle((0, -0.225), radius=0.225, color='black', fill=True)
+graph.add_patch(circle)
+
+edge_x = np.linspace(-maximum_absolute_value-0.5, maximum_absolute_value+0.5, 100)
+edge_y = abs(edge_x*1.75)
+graph.plot(edge_x, edge_y,color="blue")
+
+graph.set_xlim(-maximum_absolute_value-0.5,maximum_absolute_value+0.5)
+graph.set_ylim(-0.3,max(z_es)+1)
+
+graph.set_aspect('equal', adjustable='box')
+plt.savefig("map.png")
+print(Collided)
--- Arlo/python/utils.py
+++ Arlo/python/utils.py
@@ -4,3 +4,26 @@ def should_stop(front_dist, left_dist, right_dist):
         (left_dist != -1 and left_dist < 200) or 
         (right_dist != -1 and right_dist < 200)
     )
+
+
+import cv2 # Import the OpenCV library
+import cv2.aruco as aruco
+import numpy as np
+
+imageSize = (1640, 1232)
+
+# Prepares the detector for our used ARUCO codes
+aruco_dict = aruco.getPredefinedDictionary(cv2.aruco.DICT_6X6_250)
+parameters = aruco.DetectorParameters()
+detector = aruco.ArucoDetector(aruco_dict, parameters)
+
+# Sets the 
+focus_length = 1316
+camera_matrix = np.array([[focus_length, 0, imageSize[0]/2],
+                          [0, focus_length, imageSize[1]/2],
+                          [0,   0,   1]], dtype=np.float32)
+dist_coeffs = np.zeros((5,))
+marker_length = 0.145
+
+def find_corner_coordinates(corners):
+    return aruco.estimatePoseSingleMarkers(corners, marker_length, camera_matrix, dist_coeffs)
