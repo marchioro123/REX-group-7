8eb29db37246c0fef011f0e23200b20f56f12a47
Martin <martin.marchioro@gmail.com>
2025-09-24 15:09:10 +0200

    .

---
+++ Arlo/python/Exercises/MartinM.py
@@ -0,0 +1,111 @@
+import sys
+from time import sleep
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+import cv2
+from math import sin, cos, sqrt
+from matplotlib.animation import FFMpegWriter
+import queue
+import threading
+
+sys.path.append("..")
+from utils import find_corner_coordinates, detector
+
+from path_planning.grid_occ import GridOccupancyMap
+from path_planning.robot_models import PointMassModel
+from path_planning.rrt import RRT
+
+DISTANCE_TO_CENTER = 0.1
+BOX_RADIUS = 0.18
+ROBOT_RADIUS = 0.2250
+
+image = cv2.imread("../../../Images/3_boxes.png")
+
+corners, ids, rejected = detector.detectMarkers(image)
+rvecs, tvecs, _ = find_corner_coordinates(corners)
+
+x_es = []
+z_es = []
+obstacle_centers = []
+for i in range(len(ids)):
+    x, y, z = tvecs[i][0]
+    x_dir, _, z_dir = rvecs[i][0]
+    x_es.append(x)
+    z_es.append(z)
+    obstacle_center = (x-sin(z_dir/2)*DISTANCE_TO_CENTER, z+cos(z_dir/2)*DISTANCE_TO_CENTER)
+    obstacle_centers.append(obstacle_center)
+
+maximum_absolute_value = max(abs(x) for x in x_es)
+edge_x = np.linspace(-maximum_absolute_value-0.5, maximum_absolute_value+0.5, 100)
+edge_y = abs(edge_x*1.75)
+
+
+path_res = 0.01
+map = GridOccupancyMap(low=(-maximum_absolute_value-0.5, -0.3), high=(maximum_absolute_value+0.5, max(z_es)+1), res=path_res)
+for i in range(map.n_grids[0]):
+    for j in range(map.n_grids[1]):
+        centroid = np.array([map.map_area[0][0] + map.resolution * (i+0.5), map.map_area[0][1] + map.resolution * (j+0.5)])
+        for o in obstacle_centers:
+            if np.linalg.norm(centroid - o) <= BOX_RADIUS + ROBOT_RADIUS:
+                map.grid[i, j] = 1
+                break
+
+robot = PointMassModel(ctrl_range=[-path_res, path_res])
+
+rrt = RRT(
+    start=[0, 0],
+    goal=[0, 3],
+    robot_model=robot,
+    map=map,
+    expand_dis=1,
+    path_resolution=path_res,
+    ) 
+show_animation = True
+metadata = dict(title="RRT Test")
+writer = FFMpegWriter(fps=15, metadata=metadata)
+fig = plt.figure()
+
+with writer.saving(fig, "rrt_test.mp4", 100):
+    path = rrt.planning(animation=show_animation, writer=writer)
+
+    if path is None:
+        print("Cannot find path")
+    else:
+        print("found path!!")
+        print(path)
+        # Draw final path
+        if show_animation:
+            rrt.draw_graph()
+            plt.plot([x for (x, y) in path], [y for (x, y) in path], '-r')
+            plt.grid(True)
+            plt.pause(0.01)  # Need for Mac
+            plt.show()
+            writer.grab_frame()
+
+
+from motor_thread import MotorThread
+import robot
+arlo = robot.Robot()
+
+cmd_queue = queue.Queue()
+SERIAL_LOCK = threading.Lock()
+motor = MotorThread(arlo, cmd_queue, serial_lock=SERIAL_LOCK)
+motor.start()
+
+
+from utils import calculate_distance, calculate_turn_angle
+pos_x, pos_y, angle = 0, 0, 0
+
+for point in path:
+    target_x, target_y = point
+
+    distance = calculate_distance(pos_x, pos_y, target_x, target_y)
+    turn_angle = calculate_turn_angle(pos_x, pos_y, angle, target_x, target_y)
+    cmd_queue.put(("drive_n_cm_forward", 0, distance))
+    cmd_queue.put(("turn_n_degrees", 0, angle))
+
+    pos_x, pos_y = target_x, target_y
+    angle = (angle + turn_angle) % 360
+
+
--- Arlo/python/utils.py
+++ Arlo/python/utils.py
@@ -1,9 +1,32 @@
+import math
+
 def should_stop(front_dist, left_dist, right_dist):        
     return (
         (front_dist != -1 and front_dist < 500) or 
         (left_dist != -1 and left_dist < 200) or 
         (right_dist != -1 and right_dist < 200)
     )
+
+def calculate_distance(x1, y1, x2, y2):
+    dx = x2 - x1
+    dy = y2 - y1
+    return math.sqrt(dx**2 + dy**2) * 100
+
+def calculate_turn_angle(x1, y1, angle, x2, y2):
+    dx = x2 - x1
+    dy = y2 - y1
+    
+    # atan2 gives radians from X-axis CCW, convert to degrees and normalize to [0, 360)
+    target_angle = math.degrees(math.atan2(dy, dx)) % 360
+    
+    # Compute smallest turn (could be left or right)
+    turn_angle = (target_angle - angle) % 360
+    
+    # Choose shortest direction: convert >180° turns to negative (left turn)
+    if turn_angle > 180:
+        turn_angle -= 360
+    
+    return turn_angle  # can be negative (left) or positive (right)
 
 
 import cv2 # Import the OpenCV library
