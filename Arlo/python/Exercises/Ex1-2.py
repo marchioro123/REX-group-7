7164d20ecc209a1fd2ccae60ec06e07a1c5a7fe7
Martin <martin.marchioro@gmail.com>
2025-09-24 16:57:11 +0200

    .

--- Arlo/python/Exercises/Ex1-2.py
+++ Arlo/python/Exercises/Ex1-2.py
@@ -13,7 +13,7 @@ cmd_queue = queue.Queue()
 motor = MotorThread(arlo, cmd_queue, serial_lock=SERIAL_LOCK)
 motor.start()
 
-for _ in range(int(360/5)):
+for _ in range(72):
     cmd_queue.put(("turn_n_degrees", 5))
 
 time.sleep(999)
