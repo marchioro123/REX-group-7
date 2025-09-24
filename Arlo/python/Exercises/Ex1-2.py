6ccd0caf2e3ffc592a2038ee06e933f0cc97527d
Martin <martin.marchioro@gmail.com>
2025-09-24 15:19:39 +0200

    .

--- Arlo/python/Exercises/Ex1-2.py
+++ Arlo/python/Exercises/Ex1-2.py
@@ -1,14 +1,16 @@
 import time
 import queue
 import sys
+import threading
 sys.path.append("..")
 import robot
 from motor_thread import MotorThread
+SERIAL_LOCK = threading.Lock()
 
 arlo = robot.Robot()
 
 cmd_queue = queue.Queue()
-motor = MotorThread(arlo, cmd_queue)
+motor = MotorThread(arlo, cmd_queue, serial_lock=SERIAL_LOCK)
 motor.start()
 
 cmd_queue.put(("turn_n_degrees", 360))
