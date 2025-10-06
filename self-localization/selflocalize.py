07aca49ce5cca248e26ea0762fff73f0ea436a9d
Martin <martin.marchioro@gmail.com>
2025-10-06 16:10:10 +0200

    .

--- self-localization/selflocalize.py
+++ self-localization/selflocalize.py
@@ -186,7 +186,7 @@ try:
         # # XXX: You do this
         if all(seen.values()):
             target_x, target_y = (landmarks[8][0] + landmarks[1][0]) / 2, (landmarks[8][1] + landmarks[1][1]) / 2
-            pos_x, pos_y, est_theta = est_pose.getX(), -est_pose.getY(), -est_pose().getTheta()
+            pos_x, pos_y, est_theta = est_pose.getX(), -est_pose.getY(), -est_pose.getTheta()
            
             turn_angle = calculate_turn_angle(pos_x, pos_y, (90.0 - math.degrees(est_theta)) % 360.0, target_x, target_y)
             distance = calculate_distance(pos_x, pos_y, target_x, target_y)
