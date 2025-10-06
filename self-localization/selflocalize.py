f05d130ddd298d42727150fa56eed811705bce27
Martin <martin.marchioro@gmail.com>
2025-10-06 16:08:53 +0200

    .

--- self-localization/selflocalize.py
+++ self-localization/selflocalize.py
@@ -155,11 +155,11 @@ try:
         #cam = camera.Camera(0, robottype='macbookpro', useCaptureThread=True)
         cam = camera.Camera(1, robottype='macbookpro', useCaptureThread=False)
 
-    # arlo = robot.Robot()
-    # SERIAL_LOCK = threading.Lock()
-    # cmd_queue = queue.Queue()
-    # motor = MotorThread(arlo, cmd_queue, serial_lock=SERIAL_LOCK)
-    # motor.start()
+    arlo = robot.Robot()
+    SERIAL_LOCK = threading.Lock()
+    cmd_queue = queue.Queue()
+    motor = MotorThread(arlo, cmd_queue, serial_lock=SERIAL_LOCK)
+    motor.start()
 
     while True:
 
@@ -185,25 +185,25 @@ try:
         # # XXX: Make the robot drive
         # # XXX: You do this
         if all(seen.values()):
-            # target_x, target_y = (landmarks[8][0] + landmarks[1][0]) / 2, (landmarks[8][1] + landmarks[1][1]) / 2
-            # pos_x, pos_y = est_pose.getX(), est_pose.getY()
+            target_x, target_y = (landmarks[8][0] + landmarks[1][0]) / 2, (landmarks[8][1] + landmarks[1][1]) / 2
+            pos_x, pos_y, est_theta = est_pose.getX(), -est_pose.getY(), -est_pose().getTheta()
            
-            # turn_angle = calculate_turn_angle(pos_x, pos_y, (90.0 - math.degrees(est_pose.getTheta())) % 360.0, target_x, target_y)
-            # distance = calculate_distance(pos_x, pos_y, target_x, target_y)
-            # print(f"Turn {turn_angle:.2f}°, then go {distance:.3f} cm forward")
-            time.sleep(10000)
-            # cmd_queue.put(("turn_n_degrees", turn_angle))
-            # cmd_queue.put(("drive_n_cm_forward", 0, distance))
+            turn_angle = calculate_turn_angle(pos_x, pos_y, (90.0 - math.degrees(est_theta)) % 360.0, target_x, target_y)
+            distance = calculate_distance(pos_x, pos_y, target_x, target_y)
+            print(f"Turn {turn_angle:.2f}°, then go {distance:.3f} cm forward")
 
-            # particle.move_particles(particles, target_x-pos_x, target_y-pos_y, -math.radians(turn_angle))
+            cmd_queue.put(("turn_n_degrees", turn_angle))
+            cmd_queue.put(("drive_n_cm_forward", 0, distance))
 
-            # for k in seen:
-            #     seen[k] = False
+            particle.move_particles(particles, target_x-pos_x, target_y-pos_y, -math.radians(turn_angle))
 
-            # while (not motor.has_started() or motor.is_turning() or motor.is_driving_forward()):
-            #     time.sleep(0.1)
-            # motor.clear_has_started()
-            # print("Stopped at target")
+            for k in seen:
+                seen[k] = False
+
+            while (not motor.has_started() or motor.is_turning() or motor.is_driving_forward()):
+                time.sleep(0.1)
+            motor.clear_has_started()
+            print("Stopped at target")
 
         # else:
         #     print("Turn 50 degrees")
@@ -297,7 +297,7 @@ try:
 
 
         est_pose = particle.estimate_pose(particles) # The estimate of the robots current pose
-        print("predX = ", est_pose.getX(), ", predY = ", est_pose.getY(), ", predTheta = ", est_pose.getTheta())
+        print("predX = ", est_pose.getX(), ", predY = ", -est_pose.getY(), ", predTheta = ", -est_pose.getTheta())
 
         if showGUI:
             # Draw map
