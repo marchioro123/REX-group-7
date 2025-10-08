00d24ea92adf1f40dc5f56d65f4d3ddf63c158c7
Martin <martin.marchioro@gmail.com>
2025-10-08 11:58:37 +0200

    .

--- self-localization/selflocalize.py
+++ self-localization/selflocalize.py
@@ -256,7 +256,7 @@ try:
                         continue
                     for p in particles:
                         weight = p.getWeight()
-                        p.setWeight( norm.pdf( p.distFrom(landmarks[box_id][0], landmarks[box_id][1]) , loc=best_distances[box_id], scale=15.0) * weight )
+                        p.setWeight( norm.pdf( p.distFrom(landmarks[box_id][0], landmarks[box_id][1]) , loc=best_distances[box_id], scale=5.0) * weight )
 
                     total_weight = np.sum([p.getWeight() for p in particles])
                     if (total_weight != 0):
