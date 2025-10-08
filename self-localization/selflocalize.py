0b4a2fc05c9b1e18bf4208eeef2377cc99b3f41e
Martin <martin.marchioro@gmail.com>
2025-10-08 12:53:33 +0200

    .

--- self-localization/selflocalize.py
+++ self-localization/selflocalize.py
@@ -290,7 +290,7 @@ try:
                         #     print("dir_delta = ",((dir_delta + np.pi) % (2*np.pi) - np.pi)*180/np.pi)
                         #     print("New weight = ", norm.pdf((dir_delta + np.pi) % (2*np.pi) - np.pi, loc=0, scale=60.0/(4*j+1) * math.pi / 180), "\n")
                         
-                        p.setWeight( norm.pdf((dir_delta + np.pi) % (2*np.pi) - np.pi, loc=0, scale=10.0 * math.pi / 180) * weight )
+                        p.setWeight( norm.pdf((dir_delta + np.pi) % (2*np.pi) - np.pi, loc=0, scale=60.0/(4*j+1) * math.pi / 180) * weight )
                         k=k+1
                     
                 total_weight = np.sum([p.getWeight() for p in particles])
