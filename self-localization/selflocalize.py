684e24e5b07a600c64ed9c02e1c60978c8aaed34
Martin <martin.marchioro@gmail.com>
2025-10-08 12:21:10 +0200

    .

--- self-localization/selflocalize.py
+++ self-localization/selflocalize.py
@@ -282,11 +282,11 @@ try:
                         #     absolute_dir += 2 * math.pi
                             
                         dir_delta = absolute_dir - p.getTheta() - best_angles[box_id]
-                        print(absolute_dir)
-                        print(p.getTheta())
-                        print(best_angles[box_id])
-                        print(dir_delta)
-                        print(norm.pdf((dir_delta + np.pi) % (2*np.pi) - np.pi))
+                        print("Absolute dir ", absolute_dir)
+                        print("getTheta = ",p.getTheta())
+                        print("Best_angles = ",best_angles[box_id])
+                        print("dir_delta = ",dir_delta)
+                        print("New weight = ", norm.pdf((dir_delta + np.pi) % (2*np.pi) - np.pi, loc=0, scale=10.0 * math.pi / 180))
                         p.setWeight( norm.pdf((dir_delta + np.pi) % (2*np.pi) - np.pi, loc=0, scale=10.0 * math.pi / 180) * weight )
                     
                 total_weight = np.sum([p.getWeight() for p in particles])
