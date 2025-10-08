16c185fb3ad063f753caeca11ea6491458b5fdfd
Martin <martin.marchioro@gmail.com>
2025-10-08 15:13:14 +0200

    .

--- self-localization/selflocalize.py
+++ self-localization/selflocalize.py
@@ -244,7 +244,7 @@ try:
                     print("Object ID = ", obj_id, ", Distance = ", dists[i], ", angle = ", angles[i]*180/np.pi)
                     seen[obj_id] = True
                     if (obj_id not in best_distances.keys()) or (best_distances[obj_id] > dists[i]):
-                        best_distances[obj_id] = dists[i]
+                        best_distances[obj_id] = dists[i] + 22.5
                         best_angles[obj_id] = angles[i]
                 # XXX: Do something for each detected object - remember, the same ID may appear several times
 
