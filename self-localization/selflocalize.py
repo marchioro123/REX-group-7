e9da79590b46c0fdbdbbab2184fefb5bea0e3117
Martin <martin.marchioro@gmail.com>
2025-10-08 13:29:42 +0200

    .

--- self-localization/selflocalize.py
+++ self-localization/selflocalize.py
@@ -217,7 +217,7 @@ try:
         #     motor.clear_has_started()
         #     print("Finished turning")
 
-        for j in range(5):
+        for j in range(20):
             # time.sleep(1)
             # Fetch next frame
             colour = cam.get_next_frame()
