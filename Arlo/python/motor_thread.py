d3207bd27494d52bf4a3c77f051fdc7c637c12e0
Martin <martin.marchioro@gmail.com>
2025-10-22 12:40:00 +0200

    .

--- Arlo/python/motor_thread.py
+++ Arlo/python/motor_thread.py
@@ -67,7 +67,7 @@ class MotorThread(threading.Thread):
     def _turn_n_degrees(self, degrees: float):
         if (degrees < 0):
             LEFTSPEED, RIGHTSPEED = 55, 55
-            duration = 0.093 * abs(degrees)
+            duration = 0.0093 * abs(degrees)
             with self.serial_lock:
                 self._is_turning = True
                 self._is_drivingForward = False
@@ -77,7 +77,7 @@ class MotorThread(threading.Thread):
 
         else:
             LEFTSPEED, RIGHTSPEED = 55, 55
-            duration = 0.093 * degrees
+            duration = 0.0093 * degrees
             with self.serial_lock:
                 self._is_turning = True
                 self._is_drivingForward = False
