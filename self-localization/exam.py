e513ebb2ac465cd913f81917a125abc92701b2ea
Martin <martin.marchioro@gmail.com>
2025-10-27 14:13:37 +0100

    .

--- self-localization/exam.py
+++ self-localization/exam.py
@@ -355,7 +355,7 @@ try:
                                 landmarks[visit_order[0]][1] / 100
                             )
                         )
-                    ) - 0.2
+                    ) - 0.3
                 ]),
                 robot_model=robot_model,
                 map=occ_map,
